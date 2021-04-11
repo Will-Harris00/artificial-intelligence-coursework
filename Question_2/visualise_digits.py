48673140d844b9da9148aa7bd8202119c565cc58
Will-Harris00 <wjharris00@outlook.com>
2021-04-11 14:01:45 +0100

    Visualise digits grid

--- Question_2/visualise_digits.py
+++ Question_2/visualise_digits.py
@@ -9,5 +9,14 @@ for ax, image, label in zip(axes.flat, digits.images, digits.target):
     ax.set(xticks=[], yticks=[])
     ax.imshow(image, cmap='binary', interpolation='nearest')
     ax.set_title('Number: %i' % label)
+plt.suptitle("Visual representation of all ten digits" )
+plt.show()
+
+_, axes = plt.subplots(6, 6, figsize=(12, 12))
+for ax, image, label in zip(axes.flat, digits.images, digits.target):
+    ax.imshow(image, cmap='binary')
+    ax.set(xticks=[], yticks=[])
+    ax.set_title(label)
+plt.suptitle("Image samples shown with their true target label", fontsize=25)
 
 plt.show()
